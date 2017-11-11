3a2ae481948cc9bf03f5d0b94d3105c42e8d873c
Evgeny <jgluhov@gmail.com>
2017-11-11 17:15:56 +0300

    add data

---
+++ src/data.js
@@ -0,0 +1,9 @@
+(function (global) {
+    global.store = global.store || {};
+
+    const data = [];
+
+    d3.range(5000).forEach(value => data.push({value}));
+
+    window.store.data = data;
+})(window);
